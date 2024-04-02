6a87e26ba82ac6bd90762f21bda7abded03a9e4d
rebecavg <rebecavgallo@hotmail.com>
2024-04-02 15:42:56 -0400

    adding requested changes

--- src/models/UserProfile.js
+++ src/models/UserProfile.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose')
 
 const UserProfileSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please provide username.'],
+        minlenght: 3,
+        maxlenght: 40
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email.'],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid e-mail address.'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide password.'],
+        minlenght: 8
+    },
     name: {
         type: String,
         required: [true, 'Please provide name.']
