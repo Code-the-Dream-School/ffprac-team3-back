a332fa35510ec2d363afe0130e1dc02a634b9950
shreeup <shree912@yahoo.com>
2024-04-03 20:59:07 -0700

    auth with basic user registration and login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,9 +1,80 @@
+const User = require("../models/UserProfile");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+const jwt = require("jsonwebtoken");
 const registerUser = async (req, res) => {
-  res.send('Successfully accessing the registerUser route');
+  // res.send("Successfully accessing the registerUser route");
+  try {
+    const { username, name, email, password, agegroup, phone, address } =
+      req.body;
+    if (
+      !username ||
+      !name ||
+      !email ||
+      !password ||
+      !agegroup ||
+      !phone ||
+      !address
+    )
+      throw new BadRequestError(
+        "Please provide username, name, email, password, agegroup, phone, address"
+      );
+
+    const user = await User.create({ ...req.body });
+
+    const token = await user.createJWT();
+    res.status(StatusCodes.CREATED).json({
+      user: { name: user.getName() },
+      token,
+      message: "Registered successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ msg: "Unable to signup" });
+  }
 };
 
 const loginUser = async (req, res) => {
-  res.send('Successfully accessing the loginUser route ');
+  //res.send("Successfully accessing the loginUser route ");
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new BadRequestError("Please provide email and password");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user)
+      throw new UnauthenticatedError("Please provide valid credentials");
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect)
+      throw new UnauthenticatedError("Please provide valid credentials");
+    const token = await user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ msg: "Unable to login" });
+  }
 };
 
-module.exports = { registerUser, loginUser };
+const getCurrentUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    //attach user to job routes
+    const user = await User.findById(payload.userId).select("-password");
+    res
+      .status(StatusCodes.OK)
+      .json({ user: { name: user.getName(), isadmin: user.isadmin }, token });
+  } catch (error) {
+    //throw new UnauthenticatedError("Authentication invalid");
+    console.log(error);
+    res.status(500).json({ msg: "Authentication invalid" });
+  }
+};
+
+module.exports = { registerUser, loginUser, getCurrentUser };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const { PORT = 8000 } = process.env;
 const app = require("./app");
+require("dotenv").config();
+//connectdb
+const connectDB = require("./db/connect");
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
+const listener = async () => {
+  await connectDB(process.env.MONGO_URI);
+  console.log(`Listening on Port ${PORT}!`);
+};
 app.listen(PORT, listener);
