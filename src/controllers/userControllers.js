7820009693830ea9c68b3a2cef18bbc0fa4f4ab3
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-04-08 17:22:03 -0700

    implement mongodb dependency package. Remove unecessary required fields for UserProfile model and controllers.

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,36 +1,24 @@
-const User = require("../models/UserProfile");
-const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, NotFoundError } = require("../errors");
-const jwt = require("jsonwebtoken");
+const User = require('../models/UserProfile');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
+const jwt = require('jsonwebtoken');
 const registerUser = async (req, res) => {
-  // res.send("Successfully accessing the registerUser route");
   try {
-    const { username, name, email, password, agegroup, phone, address } =
-      req.body;
-    if (
-      !username ||
-      !name ||
-      !email ||
-      !password ||
-      !agegroup ||
-      !phone ||
-      !address
-    )
-      throw new BadRequestError(
-        "Please provide username, name, email, password, agegroup, phone, address"
-      );
-
+    const { name, email, password } = req.body;
+    if (!name || !email || !password)
+      throw new BadRequestError('Please provide name, email, and password');
+    res.send('Successfully accessing the registerUser route');
+    console.log(req.body);
     const user = await User.create({ ...req.body });
-
-    const token = await user.createJWT();
-    res.status(StatusCodes.CREATED).json({
-      user: { name: user.getName() },
-      token,
-      message: "Registered successfully",
-    });
+    //   const token = await user.createJWT();
+    //   res.status(StatusCodes.CREATED).json({
+    //     user: { name: user.getName() },
+    //     token,
+    //     message: "Registered successfully",
+    //   });
   } catch (e) {
     console.log(e);
-    res.status(500).json({ msg: "Unable to signup" });
+    res.status(500).json({ msg: 'Unable to signup' });
   }
 };
 
@@ -39,41 +27,41 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      throw new BadRequestError("Please provide email and password");
+      throw new BadRequestError('Please provide email and password');
     }
 
     const user = await User.findOne({ email });
 
     if (!user)
-      throw new UnauthenticatedError("Please provide valid credentials");
+      throw new UnauthenticatedError('Please provide valid credentials');
     const isPasswordCorrect = await user.comparePassword(password);
     if (!isPasswordCorrect)
-      throw new UnauthenticatedError("Please provide valid credentials");
+      throw new UnauthenticatedError('Please provide valid credentials');
     const token = await user.createJWT();
     res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
   } catch (e) {
     console.log(e);
-    res.status(500).json({ msg: "Unable to login" });
+    res.status(500).json({ msg: 'Unable to login' });
   }
 };
 
 const getCurrentUser = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw new UnauthenticatedError("Authentication invalid");
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid');
   }
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     //attach user to job routes
-    const user = await User.findById(payload.userId).select("-password");
+    const user = await User.findById(payload.userId).select('-password');
     res
       .status(StatusCodes.OK)
       .json({ user: { name: user.getName(), isadmin: user.isadmin }, token });
   } catch (error) {
     //throw new UnauthenticatedError("Authentication invalid");
     console.log(error);
-    res.status(500).json({ msg: "Authentication invalid" });
+    res.status(500).json({ msg: 'Authentication invalid' });
   }
 };
 
--- src/models/UserProfile.js
+++ src/models/UserProfile.js
@@ -1,55 +1,48 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const UserProfileSchema = mongoose.Schema({
-  username: {
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserProfileSchema = new mongoose.Schema({
+  name: {
     type: String,
-    required: [true, "Please provide username."],
-    minlenght: 3,
-    maxlenght: 40,
+    required: [true, 'Please provide name.'],
   },
   email: {
     type: String,
-    required: [true, "Please provide email."],
+    required: [true, 'Please provide email.'],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide valid e-mail address.",
+      'Please provide valid e-mail address.',
     ],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, "Please provide password."],
+    required: [true, 'Please provide password.'],
     minlenght: 8,
   },
-  name: {
+  username: {
     type: String,
-    required: [true, "Please provide name."],
-  },
-  agegroup: {
-    type: String,
-    enum: ["18 - 30", "31 - 40", "41 - 50", "51 - 60", "61 - 70", "71 - 80"],
-    required: [true, "Please provide age group."],
+    minlenght: 3,
+    maxlenght: 40,
   },
   email: {
     type: String,
-    ref: "UserLogon",
+    ref: 'UserLogon',
   },
   phone: {
     type: String,
-    required: [true, "Please provide phone number."],
     unique: true,
-    match: [/^[0-9]+$/, "Please provide valid phone number."],
+    match: [/^[0-9]+$/, 'Please provide valid phone number.'],
     minlenght: 10,
     maxlenght: 10,
   },
   address: {
     type: String,
-    required: [true, "Please provide address."],
   },
 });
 
-UserProfileSchema.pre("save", async function () {
+UserProfileSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
@@ -72,4 +65,4 @@ UserProfileSchema.methods.comparePassword = async function (candidatePassword) {
   const isMatch = await bcrypt.compare(candidatePassword, this.password);
   return isMatch;
 };
-module.exports = mongoose.model("UserProfile", UserProfileSchema);
+module.exports = mongoose.model('UserProfile', UserProfileSchema);
