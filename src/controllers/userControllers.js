a3b723cd808334561bf065c7c34dd192cb707382
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-04-08 18:51:01 -0700

    update loginUser to use updated name for UserProfile mongoose model.

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -24,21 +24,24 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  //res.send("Successfully accessing the loginUser route ");
+  // res.send('Successfully accessing the loginUser route ');
   try {
     const { email, password } = req.body;
     if (!email || !password) {
       throw new BadRequestError('Please provide email and password');
     }
 
-    const user = await User.findOne({ email });
+    const user = await UserProfile.findOne({ email });
 
     if (!user)
       throw new UnauthenticatedError('Please provide valid credentials');
+
     const isPasswordCorrect = await user.comparePassword(password);
     if (!isPasswordCorrect)
       throw new UnauthenticatedError('Please provide valid credentials');
+
     const token = await user.createJWT();
+
     res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
   } catch (e) {
     console.log(e);
