9acc13d8a9725993f7a427c5baccd0e253958708
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-04-09 08:55:56 -0700

    added timestamps to UserProfile model and Pet model.

--- src/models/Pets.js
+++ src/models/Pets.js
@@ -1,34 +1,37 @@
 const mongoose = require('mongoose');
 
-const PetsSchema = mongoose.Schema({
-  type: {
-    type: String,
-    require: [true, 'Please provide a pet type'],
-  },
-  age: {
-    type: String,
-  },
-  sex: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  isFavorite: {
-    type: Boolean,
-  },
-  location: {
-    type: String,
-  },
-  authorizedUsers: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'UserProfile',
+const PetsSchema = mongoose.Schema(
+  {
+    type: {
+      type: String,
+      require: [true, 'Please provide a pet type'],
     },
-  ],
-});
+    age: {
+      type: String,
+    },
+    sex: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    isFavorite: {
+      type: Boolean,
+    },
+    location: {
+      type: String,
+    },
+    authorizedUsers: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'UserProfile',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Pet', PetsSchema);
