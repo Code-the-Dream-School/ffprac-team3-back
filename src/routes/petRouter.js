d7035de8bc8b979554364a849eecb3ab55ea8ae1
rebecavg <rebecavgallo@hotmail.com>
2024-04-15 18:29:42 -0400

    adding auth back to routes

--- src/routes/petRouter.js
+++ src/routes/petRouter.js
@@ -11,9 +11,9 @@ const {
 } = require('../controllers/petController');
 
 router.route('/getAllPets').get(getAllPets);
-router.route('/createPet').post(createPet);
+router.route('/createPet').post(auth, createPet);
 router.route('/updatePet/:id').patch(auth, updatePet);
-router.route('/update/:id').patch(updateMedicalPet)
+router.route('/update/:id').patch(auth, updateMedicalPet)
 router.route('/deletePet/:id').delete(auth, deletePet);
 
 module.exports = router;
