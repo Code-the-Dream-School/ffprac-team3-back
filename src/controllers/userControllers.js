41c7c08db6c05cbd35f9e6b89f42b37dc4848abf
shreeup <shree912@yahoo.com>
2024-04-23 22:14:59 -0700

    change user model name in getcurrentuser method

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,14 +1,14 @@
-const UserProfile = require('../models/UserProfile');
-const { StatusCodes } = require('http-status-codes');
-const { BadRequestError, NotFoundError } = require('../errors');
-const jwt = require('jsonwebtoken');
+const UserProfile = require("../models/UserProfile");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
     if (!firstName || !lastName || !email || !password)
       throw new BadRequestError(
-        'Please provide first name, last name, email, and password'
+        "Please provide first name, last name, email, and password"
       );
 
     console.log({ ...req.body });
@@ -17,11 +17,11 @@ const registerUser = async (req, res) => {
 
     res.status(StatusCodes.CREATED).json({
       user: { name: user.getName() },
-      message: 'Registered successfully',
+      message: "Registered successfully",
     });
   } catch (e) {
     console.log(e);
-    res.status(500).json({ msg: 'Unable to signup' });
+    res.status(500).json({ msg: "Unable to signup" });
   }
 };
 
@@ -30,44 +30,44 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      throw new BadRequestError('Please provide email and password');
+      throw new BadRequestError("Please provide email and password");
     }
 
     const user = await UserProfile.findOne({ email });
 
     if (!user)
-      throw new UnauthenticatedError('Please provide valid credentials');
+      throw new UnauthenticatedError("Please provide valid credentials");
 
     const isPasswordCorrect = await user.comparePassword(password);
     if (!isPasswordCorrect)
-      throw new UnauthenticatedError('Please provide valid credentials');
+      throw new UnauthenticatedError("Please provide valid credentials");
 
     const token = await user.createJWT();
 
     res.status(StatusCodes.OK).json({ user: { name: user.firstName }, token });
   } catch (e) {
     console.log(e);
-    res.status(500).json({ msg: 'Unable to login' });
+    res.status(500).json({ msg: "Unable to login" });
   }
 };
 
 const getCurrentUser = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnauthenticatedError('Authentication invalid');
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnauthenticatedError("Authentication invalid");
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     //attach user to job routes
-    const user = await User.findById(payload.userId).select('-password');
+    const user = await UserProfile.findById(payload.userId).select("-password");
     res
       .status(StatusCodes.OK)
       .json({ user: { name: user.getName(), isadmin: user.isadmin }, token });
   } catch (error) {
     //throw new UnauthenticatedError("Authentication invalid");
     console.log(error);
-    res.status(500).json({ msg: 'Authentication invalid' });
+    res.status(500).json({ msg: "Authentication invalid" });
   }
 };
 
